50c09eb405f07f3a5bdafe3334d8e47e24c1bf1f
Kuro <dev_kuro@yahoo.de>
2025-06-24 12:51:40 +0200

    improved build scripts

--- buildNativeDlls.bat
+++ buildNativeDlls.bat
@@ -1,16 +1,26 @@
 @echo off
 setlocal
 
-:: Set path to MSYS2 bash
+:: Path to MSYS2 bash
 set "MSYS2_BASH=C:\msys64\usr\bin\bash.exe"
+
+:: Get directory of this script, remove trailing backslash
+set "LOCAL_DIR=%~dp0"
+set "LOCAL_DIR=%LOCAL_DIR:~0,-1%"
+
+:: Convert to MSYS2-friendly path
+set "LOCAL_DIR_UNIX=%LOCAL_DIR:\=/%"
+set "LOCAL_DIR_UNIX=/%LOCAL_DIR_UNIX:~0,1%%LOCAL_DIR_UNIX:~2%"
 
 :: Check if bash exists
 if not exist "%MSYS2_BASH%" (
-    echo "Error: %MSYS2_BASH% not found"
+    echo Error: %MSYS2_BASH% not found
+    endlocal
     exit /b 1
 )
 
-:: Run CMake commands inside MinGW bash
-"%MSYS2_BASH%" -l -c "cd /c/Users/Kuro/IdeaProjects/JNI-Test && ./buildNativeDlls.sh"
+:: Run build script inside MSYS2 bash
+"%MSYS2_BASH%" -l -c "cd \"%LOCAL_DIR_UNIX%\" && ./buildNativeDlls.sh"
 
 endlocal
+exit /b 0
--- buildNativeDlls.sh
+++ buildNativeDlls.sh
@@ -6,7 +6,6 @@ cd native
 
 cmake -B build -G "MinGW Makefiles" \
   -DCMAKE_C_COMPILER=gcc \
-  -DCMAKE_CXX_COMPILER=g++ \
   -DCMAKE_MAKE_PROGRAM=mingw32-make
 
 cmake --build build
