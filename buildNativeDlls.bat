c32b23247978d0a17d786f0e2c0ac140a9887f67
Kuro <dev_kuro@yahoo.de>
2025-06-27 17:07:52 +0200

    Adjusted build script

--- buildNativeDlls.bat
+++ buildNativeDlls.bat
@@ -1,20 +1,15 @@
 @echo off
 setlocal
 
-:: Path to MSYS2 bash
-set "MSYS2_BASH=C:\msys64\usr\bin\bash.exe"
-
-:: Get directory of this script, remove trailing backslash
 set "LOCAL_DIR=%~dp0"
 set "LOCAL_DIR=%LOCAL_DIR:~0,-1%"
 
-:: Convert to MSYS2-friendly path
 set "LOCAL_DIR_UNIX=%LOCAL_DIR:\=/%"
 set "LOCAL_DIR_UNIX=/%LOCAL_DIR_UNIX:~0,1%%LOCAL_DIR_UNIX:~2%"
 
 :: Check if bash exists
 if not exist "%MSYS2_BASH%" (
-    echo Error: %MSYS2_BASH% not found
+    echo Error: Variable MSYS2_BASH not found
     endlocal
     exit /b 1
 )
