6384bc3bffaefdc15ee13d289391369c3bdf58f1
Kuro <dev_kuro@yahoo.de>
2025-08-30 13:08:13 +0200

    Added documentation to Decal.java

--- src/main/java/org/kurodev/jpixelgameengine/gfx/decal/Decal.java
+++ src/main/java/org/kurodev/jpixelgameengine/gfx/decal/Decal.java
@@ -2,6 +2,7 @@ package org.kurodev.jpixelgameengine.gfx.decal;
 
 import org.kurodev.jpixelgameengine.gfx.sprite.Sprite;
 import org.kurodev.jpixelgameengine.impl.ffm.NativeFunction;
+import org.kurodev.jpixelgameengine.impl.ffm.PixelGameEngine;
 import org.kurodev.jpixelgameengine.pos.FloatVector2D;
 import org.kurodev.jpixelgameengine.pos.Vector2D;
 import org.slf4j.Logger;
@@ -12,9 +13,11 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.lang.ref.Cleaner;
 
+/**
+ * Decals Should not be created outside of {@link PixelGameEngine#onUserCreate()} or {@link PixelGameEngine#onUserUpdate(float)}
+ */
 public class Decal {
     private static final Logger log = LoggerFactory.getLogger(Decal.class);
-    //TODO Check if the Cleaner is actually getting triggered at some point or if the reference is still too hard.
     private static final Cleaner CLEANER = Cleaner.create();
     private static final NativeFunction<MemorySegment> CREATE_DECAL = new NativeFunction<>("decal_create", ValueLayout.ADDRESS, ValueLayout.ADDRESS);
     private static final NativeFunction<Void> DESTROY_DECAL = new NativeFunction<>("decal_destroy", FunctionDescriptor.ofVoid(ValueLayout.ADDRESS));
@@ -28,12 +31,12 @@ public class Decal {
     public Decal(Sprite sprite) {
         ptr = CREATE_DECAL.invokeExact(m -> m, sprite.getSpritePtr());
         this.sprite = sprite;
-   registerCleaner();
+        registerCleaner();
     }
 
     private void registerCleaner() {
         final MemorySegment finalSpritePtr = ptr;
-    final Sprite finalSprite = sprite;
+        final Sprite finalSprite = sprite;
         CLEANER.register(this, () -> {
             log.info("Unloading Decal {}", finalSprite.getName());
             try {
@@ -48,10 +51,18 @@ public class Decal {
         return ptr;
     }
 
+    /**
+     * Update reads the texture from the provided sprite into the GPU.
+     * This will update the Decal to match the Sprite
+     */
     public void update() {
         DECAL_UPDATE.invoke(ptr);
     }
 
+    /**
+     * UpdateSprite Writes the Decal texture into the provided sprite.
+     * This will update the sprite to match the decal
+     */
     public void updateSprite() {
         DECAL_UPDATE_SPRITE.invoke(ptr);
     }
